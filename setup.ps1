7b8db048a21479667345ad6af7cf7bc4dd19e339
lliwi <llibert.morreres@gmail.com>
2021-04-11 13:14:33 +0200

    setup

---
+++ setup.ps1
@@ -0,0 +1,27 @@
+﻿#check the current execution policy
+$policy = Get-ExecutionPolicy
+
+#disable av
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+if ($policy -eq "Restricted") {
+    Set-ExecutionPolicy Unrestricted
+}
+
+#unblock all the files
+Get-ChildItem . -Recurse | Unblock-File
+
+# Import modules
+Import-Module .\PowerSploit\PowerSploit.psm1 
+Import-Module .\nishang\nishang.psm1
+Import-Module .\Posh-SecMod\Posh-SecMod.psm1
+Import-Module .\Posh-SecMod\Audit\Audit.psm1
+Import-Module .\Posh-SecMod\Database\Database.psm1
+Import-Module .\Posh-SecMod\Discovery\Discovery.psm1
+Import-Module .\Posh-SecMod\Parse\Parse.psm1
+Import-Module .\Posh-SecMod\PostExploitation\PostExploitation.psm1
+Import-Module .\Posh-SecMod\Registry\Registry.psm1
+Import-Module .\Posh-SecMod\Utility\utility.psm1
+
+Import-Module .\Posh-Shodan\Posh-Shodan.psm1
+
